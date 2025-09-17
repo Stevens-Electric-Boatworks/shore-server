a0c94c12b14f3db72d661a350a2ac91ffcbce794
Thiago <tja.usa.202@gmail.com>
2025-09-17 19:33:58 -0400

    Upload data to database

--- src/socket.ts
+++ src/socket.ts
@@ -2,11 +2,56 @@ import { createServer } from "http";
 import { WebSocketServer, WebSocket } from "ws";
 import app from "./app";
 import { randomUUID } from "crypto";
+import { db } from "./lib/db";
 
 const server = createServer(app);
 const wss = new WebSocketServer({ server });
 
 const clients = new Map<string, WebSocket>();
+
+type DataReading = {
+  id: number;
+  timestamp: Date;
+  vbat?: number;
+  vebat?: number;
+  temp_bat?: number;
+  current_bat?: number;
+  coolant_temp?: number;
+  rpm_a?: number;
+  rpm_b?: number;
+  motor_synch?: number;
+  speed?: number;
+  heading?: number;
+  gps_lat?: number;
+  gps_lon?: number;
+  gps_alt?: number;
+  imu_x?: number;
+  imu_y?: number;
+  imu_z?: number;
+  propulsion_angle?: number;
+  bms_temp?: number;
+  bat_comp_temp?: number;
+  throttle?: number;
+  battery_percent?: number;
+  can_bus_util_percent?: number;
+};
+
+let buffer: DataReading[] = [];
+
+setInterval(async () => {
+  if (buffer.length <= 0) return;
+
+  try {
+    console.log("Uploading to DB...");
+    const res = await db.dataReading.createMany({
+      data: buffer,
+    });
+
+    buffer = [];
+  } catch (err) {
+    console.error("Failed to upload data to database:", err);
+  }
+}, 3000);
 
 wss.on("connection", (ws) => {
   const id = randomUUID();
@@ -37,14 +82,19 @@ wss.on("connection", (ws) => {
       }
 
       if (parsed.type === "data" || parsed.type === "alarm") {
-        console.log("Data: ", JSON.stringify(parsed.payload));
-
         for (const [id, ws] of clients) {
           ws.send(
             JSON.stringify({
               ...parsed,
             })
           );
+        }
+
+        if (parsed.type === "data") {
+          buffer.push({
+            ...parsed.payload,
+            timestamp: new Date(),
+          });
         }
       }
     } catch (err) {
