5fc63c55da3d8bafd9581b44ef80f417a8eba6a6
Thiago Andrade <tja.usa.202@gmail.com>
2026-04-20 22:21:34 -0400

    Add refresh route test suite

--- src/__test__/utils.ts
+++ src/__test__/utils.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import { db } from "@/lib/db";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { User } from "@/types";
 
 export const mockFindFirst = db.user.findFirst as jest.Mock;
 export const mockCreate = db.user.create as jest.Mock;
@@ -11,8 +12,8 @@ export const mockBcryptCompare = bcrypt.compare as jest.Mock;
 export const mockBcryptHash = bcrypt.hash as jest.Mock;
 export const mockJwtSign = jwt.sign as jest.Mock;
 
-export const mockReqRes = (body: object) => {
-  const req = { body } as Request;
+export const mockReqRes = (body?: object, extras: Partial<Request> = {}) => {
+  const req = { body, ...extras } as Request;
   const res = {
     status: jest.fn().mockReturnThis(),
     json: jest.fn().mockReturnThis(),
@@ -20,11 +21,11 @@ export const mockReqRes = (body: object) => {
   return { req, res };
 };
 
-export const fakeUser = {
+export const fakeUser: User = {
   id: "user-123",
   username: "alice",
   password: "hashed_password",
   role: "USER",
-  refreshToken: null,
-  refreshTokenCreatedAt: null,
+  refreshToken: undefined,
+  refreshTokenCreatedAt: undefined,
 };
